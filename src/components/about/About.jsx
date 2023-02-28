a9b02f5b2e397cdc4eb4f41b1ab394f2f32f6c4a
Mark D <markaDietr@gmail.com>
2023-02-28 17:06:42 -0500

    more ajustments to text section in about me

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -35,7 +35,7 @@ const About = () => {
             Hi there! I'm Mark, a front-end developer with a passion for coding. I first discovered my love for programming when I was working as a machine operator in my second job at the age of 23. Since then, I've been hooked on the constant learning and challenges that coding presents.
             <br />
             <br />
-            In January 2022, I decided to expand my skillset and began learning JavaScript and React.js on Codecademy. Since then, I've been honing my front-end development skills and working on various projects to gain hands-on experience.
+            In January 2022, I decided to expand my skill set and began learning JavaScript and React.js on Codecademy. Since then, I've been honing my front-end development skills and working on various projects to gain hands-on experience.
             <br />
             <br />
             My current specialization is in front-end development, but I'm always looking to pick up new skills and experiences. Whether it's learning a new framework or diving deeper into UX design, I'm always eager to expand my knowledge and grow as a developer.
