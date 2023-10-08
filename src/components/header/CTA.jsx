310495848aef621afc29d98284194df89c94666c
Mark Dietrich <MarkADietr@gmail.com>
2023-10-08 13:56:12 -0400

    -remove resume

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import Resume from ''
+
 
 const CTA = () => {
   return (
     <div className='cta'>
-        <a href={Resume} download className='btn'>Download Resume</a>
+        <a download className='btn'>Download Resume</a>
         <a href="tel:7172082175" className='btn btn-primary'>Call Me</a>
     </div>
   )
